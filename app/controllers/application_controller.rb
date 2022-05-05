6e291922435249b4e0f6b0fcfda12f171008f552
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-05-05 13:12:18 +0000

    [Add]ログイン機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に
+  # configure_permitted_parametersメソッドが実行されるようになる。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # privateは記述をしたコントローラ内でしか参照できないが、
+  # protectedは呼び出された他のコントローラからも参照することが可能。
+  protected
+
+  # ユーザー登録（sign_up)の際に、ユーザー名（name）のデータ操作を許可
+  # ストロングパラメータと同様の機能
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
